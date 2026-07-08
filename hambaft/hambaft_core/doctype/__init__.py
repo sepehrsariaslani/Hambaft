673070ce925d719636562b177e1b7f84987b7b78
Sepehr Sariaslani <sepehr@hambaft.local>
2026-07-08 19:09:19 +0000

    fix(doctype): remove nested hambaft_core module causing migrate failure
    - Change module of 5 DocTypes from 'Hambaft Core' to 'Hambaft' so
      Frappe can find them during sync:
      Hambaft Finance Entry, Hambaft Note, Hambaft Note Link,
      Hambaft Project, Hambaft Task.
    - Remove nested hambaft_core/ folder and hambaft_core_hooks.py
      which duplicated DocTypes and referenced a non-existent module.
      This was causing: DoesNotExistError: Module Hambaft Core not found.

--- hambaft/hambaft_core/doctype/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from .hambaft_task import HambaftTask
-from .hambaft_habit import HambaftHabit
-from .hambaft_goal import HambaftGoal
-from .hambaft_finance_entry import HambaftFinanceEntry
-from .hambaft_project import HambaftProject
--- hambaft/hambaft_core/hooks.py
+++
@@ -1,7 +0,0 @@
-from . import __version__ as app_version
-
-app_name = "hambaft_core"
-app_title = "Hambaft Core"
-app_publisher = "Sepehr"
-app_description = "Core module for Hambaft application"
-app_version = app_version
